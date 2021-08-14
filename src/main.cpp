f0eb5bf19b00ebe829bd5dd045ef924a0ba99193
Ryan Ebrahimi <ryan_ebrahimi@yahoo.com>
2021-08-14 17:08:23 -0400

    ORB detection and compaing last image to current

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,74 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/imgproc.hpp>
+
 #include <iostream>
+
 using namespace cv;
-int main()
-{
-    std::string image_path = samples::findFile("starry_night.jpg");
-    Mat img = imread(image_path, IMREAD_COLOR);
-    if(img.empty())
-    {
-        std::cout << "Could not read the image: " << image_path << std::endl;
-        return 1;
+
+int main() {
+  /*std::string image_path;
+  std::cin >> image_path;
+  //image_path=samples::findFile(image_path); //Comment this line out for absolute image path
+  Mat img = imread(image_path, IMREAD_COLOR);
+    if(img.empty()) {
+      std::cout << "Could not read the image: " << image_path << std::endl;
+      return 1;
+  }*/
+  VideoCapture cap;
+  cap.open(0);
+
+  Mat lastFrame, lastDesc;
+  std::vector<KeyPoint> lastKeyPoints;
+
+  while(true) {
+    Mat frame;
+    cap >> frame;
+
+    auto orb = ORB::create();
+
+    std::vector<KeyPoint> points;
+    orb->detect(frame, points);
+
+    Mat descriptors;
+    orb->compute(frame, points, descriptors);
+
+    if (lastFrame.empty()) {
+      lastFrame = frame;
+      lastDesc = descriptors;
+      lastKeyPoints = points;
+
+      continue;
     }
-    imshow("Display window", img);
-    int k = waitKey(0); // Wait for a keystroke in the window
-    if(k == 's')
-    {
-        imwrite("starry_night.png", img);
-    }
-    return 0;
+
+    BFMatcher matcher(NORM_HAMMING, true);
+    std::vector<DMatch> matches;
+
+    matcher.match(descriptors, lastDesc, matches);
+
+    sort(matches.begin(), matches.end(),
+      [](const DMatch & a, const DMatch & b) -> bool {
+        return a.distance < b.distance;
+      });
+
+    if (matches.size() < 10) continue;
+
+    std::vector<DMatch>::const_iterator first = matches.begin();
+    std::vector<DMatch>::const_iterator last = matches.begin() + 10;
+    std::vector<DMatch> bestMatches(first, last);
+
+    Mat outputImage;
+    //drawKeypoints(frame, points, outputImage);
+    drawMatches(frame, points, lastFrame, lastKeyPoints, bestMatches, outputImage);
+
+    imshow("Matches", outputImage);
+
+    if (waitKey(100) == 27) return 0;
+
+    lastFrame=frame;
+    lastDesc=descriptors;
+    lastKeyPoints=points;
+  }
 }
